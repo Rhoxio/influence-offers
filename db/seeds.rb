1b25b1ea6e5e03d4d02d0c0da2711a9dab128f99
Kevin Maze <rhoxiodbc@gmail.com>
2023-12-12 16:50:01 -0800

    added in my standard string patches and got tags seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,29 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Colors come from config/intializers/string_patches.rb
+
+# Seed Offers
+# Seed Tags
+# Associate Offers with 2 Tags each
+
+def bar
+  Array.new(50, "-").join.green
+end
+
+if Rails.env.development?
+
+  puts bar
+  puts "Running seeds in Development Environment...".green
+  puts bar
+
+  puts "Creating Tags...".blue
+  tag_names = ["Premium Currency", "Cash for Play", "Referral", "Free Play Time", "In-Game Reward", "App Testing"]
+  tag_names.each do |tag_name|
+    if !Tag.where(name: tag_name).present?
+      Tag.create!(name: tag_name) 
+      puts "Created Tag: #{tag_name}".blue
+    else
+      puts "Skipped Tag: #{tag_name}".yellow
+    end
+  end
+  puts bar
+
+end
