fa9f8dbf86d30b1931286ce0dc6d1ed8ab538247
Kevin Maze <rhoxiodbc@gmail.com>
2023-12-14 22:16:22 -0800

    added in the error component for responses from the API/other areas of React

--- app/javascript/bundles/Discover/components/Offer.jsx
+++ app/javascript/bundles/Discover/components/Offer.jsx
@@ -1,26 +1,58 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 
-const Offer = ({offer, tags, show}) =>{
+const Offer = ({offer, tags, show, setError}) =>{
   // console.log(tags)
   const unclaimedButtonData = {text: "Claim", disabled: false, claimed: false}
   const claimedButtonData = {text: "Claimed!", disabled: true, claimed: true}
   const pendingButtonData = {text: "Claiming...", disabled: true, claimed: false}
   const [buttonState, setButtonState] = useState(unclaimedButtonData)
 
-  const claimClickHandler = (event) =>{
+  const claimClickHandler = (event) => {
     event.preventDefault()
-    // console.log(event)
-    // event.target.disabled = true
-    // event.target.innerHTML = "Claimed!"
-    postClaimOffer()
+    const response = postClaimOffer()
+    response.then((res) => (
+      handleClaimResponse(res)
+    ))
   }
 
-  const postClaimOffer = async () =>{
+  const delegateButtonState = (stateToSet) => {
+    const buttonStates = {
+      "claimed": claimedButtonData,
+      "pending": pendingButtonData,
+      "unclaimed": unclaimedButtonData
+    }
+    setButtonState(buttonStates[stateToSet])
+  }
+
+  const handleClaimResponse = (res) =>{
+    if(res.status){
+      switch(res.status){
+        case '409':
+          setError('You have already claimed this offer.')
+          delegateButtonState("claimed")
+          break;
+        case '401':
+          setError('Sign in again to continue.')
+          delegateButtonState("unclaimed")
+          break;
+        case '403':
+          setError('An error has occured. Refresh the page and try again.')
+          delegateButtonState("unclaimed")
+          break;
+        default:
+          setError(res.message)
+      }
+    } else {
+      delegateButtonState("claimed")
+      return res
+    }
+  }
+
+  const postClaimOffer = async (event) =>{
     const location = `${window.location.protocol}//${window.location.host}`
     const url = `${location}/api/v1/offers/${offer.id}/claim`
     const csrf = document.querySelector('meta[name="csrf-token"]').getAttribute("content")
-    console.log(url)
     const body = {
       method: "POST",
       headers: {
@@ -29,11 +61,12 @@ const Offer = ({offer, tags, show}) =>{
       }
     }
     try {
+      delegateButtonState("pending")
       const response = await fetch(url, body)
       const data = await response.json();
       return data
-    } catch(e) {
-      return e
+    } catch (error) {
+      return error
     }
   }
 
