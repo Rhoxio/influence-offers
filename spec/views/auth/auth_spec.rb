b2d5ab742edc57c3dccf6cc555190dbce6204aeb
Kevin Maze <rhoxiodbc@gmail.com>
2023-12-14 16:09:16 -0800

    got baseline tests set up for auth. looking to merge and branch for AJAX/API functionality next

--- spec/views/auth/auth_spec.rb
+++ spec/views/auth/auth_spec.rb
@@ -62,4 +62,56 @@ RSpec.describe "front page", type: :feature do
     end      
   end
 
+  context "sessions" do 
+    before(:each) do
+      @player = FactoryBot.create(:new_player)
+      FactoryBot.create(:new_offer)
+      visit "/players/sign_in"      
+    end
+
+    context "sign in" do 
+      it "will sign in" do 
+        within('#new_player') do 
+          fill_in 'Email', with: @player.email
+          fill_in 'Password', with: 'password'
+        end
+        click_button 'SIGN IN'
+        expect(page).to have_content 'Signed in successfully.' 
+      end
+
+      it "will not sign in with bad email" do 
+        within('#new_player') do 
+          fill_in 'Email', with: "bad"
+          fill_in 'Password', with: 'password'
+        end
+        click_button 'SIGN IN'
+        expect(page).to have_content 'Invalid Email or password.' 
+      end  
+
+      it "will not sign in with bad password" do 
+        within('#new_player') do 
+          fill_in 'Email', with: @player.email
+          fill_in 'Password', with: 'BADPASSWORDISBAD'
+        end
+        click_button 'SIGN IN'
+        expect(page).to have_content 'Invalid Email or password.' 
+      end       
+    end
+
+    context "sign out" do 
+
+      it "will sign out" do 
+        within('#new_player') do 
+          fill_in 'Email', with: @player.email
+          fill_in 'Password', with: 'password'
+        end
+        click_button 'SIGN IN'
+        expect(page).to have_content 'Signed in successfully.'
+        click_button 'SIGN OUT'
+        expect(page).to have_content 'Signed out successfully.'
+      end    
+    end    
+  end
+
+
 end
