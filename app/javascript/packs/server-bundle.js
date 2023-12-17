8ccaf10402bd0d9cb115425a81852c999f8bd409
Kevin Maze <rhoxiodbc@gmail.com>
2023-12-16 18:05:04 -0800

    started removing the hello_world stuff, need to branch just in case

--- app/javascript/packs/server-bundle.js
+++ app/javascript/packs/server-bundle.js
@@ -1,8 +1,10 @@
 import ReactOnRails from 'react-on-rails';
 
-import HelloWorld from '../bundles/HelloWorld/components/HelloWorldServer';
+import Discover from '../bundles/Discover/components/Discover';
+import Offer from '../bundles/Discover/components/Offer';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-  HelloWorld,
+  Discover,
+  Offer
 });
