5bef8c62db86d8fcc2606a5fc1a1f7979d662f76
Kevin Maze <rhoxiodbc@gmail.com>
2023-12-18 02:27:34 -0800

    added in validator for age_range

--- app/models/validators/offer_target_age_range_validator.rb
+++ app/models/validators/offer_target_age_range_validator.rb
@@ -1,13 +1,15 @@
 class OfferTargetAgeRangeValidator < ActiveModel::Validator
   def validate(offer)
     if offer.min_age && offer.max_age && offer.target_age
+      
       is_in_range = (offer.min_age <= offer.target_age ) && (offer.target_age <= offer.max_age)
       return true if is_in_range
+
       offer.errors.add :base, 
-      "Offer must have a target_age within the range of min_age and max_age:
-      target_age: #{offer.target_age}
-      min_age: #{offer.min_age}
-      max_age: #{offer.max_age}"
+        "Offer must have a target_age within the range of min_age and max_age:
+        target_age: #{offer.target_age}
+        min_age: #{offer.min_age}
+        max_age: #{offer.max_age}"
     end
   end  
 end
